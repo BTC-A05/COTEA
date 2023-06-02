e30f5b9d165b78670dfccf5b59936a4e85a1226a
Bùi Công Tiến <95754484+CongTienTea24@users.noreply.github.com>
2023-06-03 00:52:59 +0700

    Add files via upload

---
+++ js/autoplaymusic.js
@@ -0,0 +1,43 @@
+var collection=[];// final collection of sounds to play
+var loadedIndex=0;// horrible way of forcing a load of audio sounds
+
+// remap audios to a buffered collection
+function init(audios) {
+  for(var i=0;i<audios.length;i++) {
+    var audio = new Audio(audios[i]);
+    collection.push(audio);
+    buffer(audio);
+  }
+}
+
+// did I mention it's a horrible way to buffer?
+function buffer(audio) {
+  if(audio.readyState==4)return loaded();
+  setTimeout(function(){buffer(audio)},100);
+}
+
+// check if we're leady to dj this
+function loaded() {
+  loadedIndex++;
+  if(collection.length==loadedIndex)playLooped();
+}
+
+// play and loop after finished
+function playLooped() {
+  var audio=Math.floor(Math.random() * (collection.length));
+  audio=collection[audio];
+  audio.play();
+  setTimeout(playLooped,audio.duration*1000);
+}
+
+// the songs to be played!
+init([
+  'audio/mix8.mp3',
+  'audio/mix33.mp3',
+  'audio/musicsad.mp3',
+  'audio/mix5.mp3',
+  'audio/mix6.mp3',
+  'audio/mix 7.mp3',
+  'audio/mix9.mp3',
+  'audio/mix43.mp3'
+]);
